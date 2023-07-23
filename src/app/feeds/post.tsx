9ca82ac0a8e47abd1e294e0d4cc01382dd1b5d3b
Vincent <thisoneis4business@gmail.com>
2023-07-24 01:35:01 +1200

    feeds preview

--- src/app/feeds/post.tsx
+++ src/app/feeds/post.tsx
@@ -1,10 +1,70 @@
 import { rc } from "@d-exclaimation/next";
-import type { ReactNode } from "react";
 
 type Props = {
-  children: ReactNode;
+  user: {
+    image: string;
+    name: string;
+  };
+  time: string;
+  content: string;
+  stats?: {
+    kind: "kcal" | "km" | "mins" | "points";
+    value: number;
+  }[];
+  image?: string;
+  reactions?: {
+    emoji: string;
+    count: number;
+  }[];
 };
 
-export default rc<Props>(({ children }) => {
-  return <div>{children}</div>;
+export default rc<Props>(({ content, user, time, image, stats, reactions }) => {
+  return (
+    <div className="flex flex-col gap-2 pb-4">
+      <div className="w-full flex items-center gap-2 -translate-x-8">
+        <img
+          className="aspect-square w-8 h-8 rounded-full ring-6 ring-white"
+          src={user.image}
+          alt="profile"
+        />
+        <span className="font-medium">{user.name}</span>
+        <span className="font-light text-sm">{time}</span>
+      </div>
+      <p className="">{content}</p>
+      {stats && (
+        <div className="flex flex-col py-1 gap-2">
+          {stats.map((stat, i) => (
+            <div key={`stat-${i}`} className="flex items-center gap-1">
+              <img
+                className="aspect-square w-6 h-6 mr-1"
+                src={`/${stat.kind}.svg`}
+                alt={stat.kind}
+              />
+              <span className="font-bold text-lg">{stat.value}</span>
+              <span className="font-light text-sm">{stat.kind}</span>
+            </div>
+          ))}
+        </div>
+      )}
+      {image && (
+        <img
+          className="w-auto h-auto block aspect-ratio-auto max-w-[300px] py-1"
+          src={image}
+        />
+      )}
+      {reactions && (
+        <div className="flex items-center gap-2">
+          {reactions.map((reaction, i) => (
+            <button
+              key={`reaction-${i}`}
+              className="flex items-center gap-1 py-0.5 px-2 rounded-lg bg-black/5 hover:bg-black/10 transition-all"
+            >
+              <span className="text-sm">{reaction.emoji}</span>
+              <span className="font-semibold text-sm">{reaction.count}</span>
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "@saturday/night",
   description: "Planned features of Saturday",
+  // metadataBase: new URL("https://night.saturday.team"),
+  icons: {
+    icon: [
+      "/favicon.ico",
+      { sizes: "16x16", url: "/favicon-16x16.png" },
+      { sizes: "32x32", url: "/favicon-32x32.png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  viewport: {
+    initialScale: 1,
+    width: "device-width",
+    viewportFit: "cover",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default layout(({ children }) => {
