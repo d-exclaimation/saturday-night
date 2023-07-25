90ddb793a2c7dde00fc0f3d0e39de10e098402db
d-exclaimation <70748917+d-exclaimation@users.noreply.github.com>
2023-07-25 22:19:57 +1200

    Update page.tsx

--- src/app/connections/page.tsx
+++ src/app/connections/page.tsx
@@ -74,7 +74,7 @@ export default page(() => {
                   className="group w-full flex items-center py-2 px-3 rounded border border-black/5 gap-3 hover:bg-slate-100 active:bg-slate-100"
                   key={friend.name}
                 >
-                  <div className="w-10 h-10 md:w-12 md:h-12 border rounded-full overflow-hidden">
+                  <div className="shrink-0 w-10 h-10 md:w-12 md:h-12 border rounded-full overflow-hidden">
                     <img
                       className="w-10 h-10 md:w-12 md:h-12 rounded-full group-hover:scale-110"
                       src={`https://api.dicebear.com/6.x/shapes/svg?seed=${friend.name}`}
@@ -113,13 +113,13 @@ export default page(() => {
                   className="group w-full flex items-center py-2 px-3 rounded border border-black/5 gap-3 hover:bg-slate-100 active:bg-slate-100"
                   key={team.name}
                 >
-                  <div className="w-10 h-10 md:w-12 md:h-12 border rounded-full overflow-hidden">
+                  <div className="shrink-0 w-10 h-10 md:w-12 md:h-12 border rounded-full overflow-hidden">
                     <img
                       className="w-10 h-10 md:w-12 md:h-12 rounded-full group-hover:scale-110"
                       src={`https://api.dicebear.com/6.x/shapes/svg?seed=${team.name}`}
                     />
                   </div>
-                  <div className="flex flex-col w-full">
+                  <div className="flex flex-col max-w-[60%]">
                     <span className="max-w-[100%] truncate">{team.name}</span>
                     <span className="max-w-[100%] text-sm text-black/50 truncate">
                       {team.sport}
