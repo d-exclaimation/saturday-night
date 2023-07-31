6010dbc177886195a41e473ebe49d9849dd6f68c
Vincent <thisoneis4business@gmail.com>
2023-08-01 06:09:33 +1200

    feat: removed google fonts

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,19 +1,9 @@
 import presetAttributify from "@unocss/preset-attributify";
 import presetUno from "@unocss/preset-uno";
-import presetWebFonts from "@unocss/preset-web-fonts";
 import { defineConfig } from "unocss";
 
 export default defineConfig({
-  presets: [
-    presetUno(),
-    presetAttributify(),
-    presetWebFonts({
-      provider: "google",
-      fonts: {
-        sans: "Roboto",
-      },
-    }),
-  ],
+  presets: [presetUno(), presetAttributify()],
   rules: [
     ["animate-bounce", { animation: "bounce 1s infinite backwards" }],
     [
