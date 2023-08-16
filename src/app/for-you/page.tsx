4f80a3b2244596707a75eaf572ee119f85352840
Vincent <thisoneis4business@gmail.com>
2023-08-16 20:13:21 +1200

    fix

--- src/app/for-you/page.tsx
+++ src/app/for-you/page.tsx
@@ -6,7 +6,7 @@ import Emoji from "../feeds/emoji";
 export default page(() => {
   return (
     <div className="w-full min-h-[100dvh]  flex items-center justify-center">
-      <div className="w-full md:w-3xl min-h-full flex flex-col items-start justify-start md:my-2 my-8 py-2 bg-white rounded-lg shadow-md">
+      <div className="w-full md:w-3xl min-h-full flex flex-col items-start justify-start md:my-2 md:my-8 py-2 bg-white rounded-lg shadow-md">
         <div className="w-full flex items-center justify-center py-3 bg-white/75 backdrop-blur sticky top-0 z-20">
           <h2 className="text-2xl font-semibold">For you</h2>
         </div>
