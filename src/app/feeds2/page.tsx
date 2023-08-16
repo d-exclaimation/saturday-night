2c75d37c9501b54979aabce56680d9718239a776
Vincent <thisoneis4business@gmail.com>
2023-08-16 21:00:06 +1200

    ye

--- src/app/feeds2/page.tsx
+++ src/app/feeds2/page.tsx
@@ -8,7 +8,7 @@ export default page(() => {
     <div className="w-full min-h-[100dvh]  flex items-center justify-center">
       <div className="w-full md:w-3xl min-h-full flex flex-col items-start justify-start md:my-2 md:my-8 py-2 bg-white rounded-lg shadow-md">
         <div className="w-full flex items-center justify-center py-3 bg-white/75 backdrop-blur sticky top-0 z-20">
-          <h2 className="text-2xl font-semibold">For you</h2>
+          <h2 className="text-2xl font-semibold">Feeds</h2>
         </div>
 
         <div className="w-[calc(100%-3.5rem)] mx-7 px-1 pb-10 border-l border-black/15 flex flex-col items-start justify-start">
